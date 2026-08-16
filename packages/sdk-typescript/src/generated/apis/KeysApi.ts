22dda1d02be6965723e8c30cf1b96f7351472cbd
dementive <87823030+dementive@users.noreply.github.com>
2026-08-16 17:46:06 +0000

    chore: update Engine API contract and SDKs

--- packages/sdk-typescript/src/generated/apis/KeysApi.ts
+++ packages/sdk-typescript/src/generated/apis/KeysApi.ts
@@ -36,6 +36,15 @@ export class KeysApi extends runtime.BaseAPI {
 
     const headerParameters: runtime.HTTPHeaders = {}
 
+    if (this.configuration && this.configuration.accessToken) {
+      const token = this.configuration.accessToken
+      const tokenString = await token('ApiKeyAuth', [])
+
+      if (tokenString) {
+        headerParameters['Authorization'] = `Bearer ${tokenString}`
+      }
+    }
+
     let urlPath = `/v1/keys/validate`
 
     return {
